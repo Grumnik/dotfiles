bcc124d8dcd0cf032c4ff0453d5999360f40bed4
Mick <mick@highbiza.nl>
2022-06-19 10:54:01 +0200

    add install_scripts

--- Makefile
+++ Makefile
@@ -1,21 +1,32 @@
-#.PHONY: ask
+ifeq (${XDG_DATA_HOME},"")
+	XDG_DATA_HOME="$$HOME/.config"
+endif
+SHELL_INIT="$$HOME/.bashrc"
 
 all:
 	@echo "no argument passed do 'make help' for examples"
 
 help:
-	@echo "possible arguments: config_i3, config_nvim" 
+	@echo "possible arguments: show_vars, config_i3, config_nvim, install_scripts" 
+
+show_vars:
+	@echo "SHELL_INIT: ${SHELL_INIT}"
+	@echo "XDG_DATA_HOME: ${XDG_DATA_HOME}"
 
 question:
 	@echo "This wil override your current config, do you want to continue?" && read ans && [ $$ans = y ]
 
 config_i3: question
-	cp -r ./i3 ~/.config
+	cp -r ./i3 ${XDG_DATA_HOME}
 
 config_nvim: question
 	# cp dir, install vim-plug, install plugins
 	cp -r ./nvim ~/.config
-	curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-	nvim -es -u ~/.config/nvim/init.vim -i NONE -c "PlugInstall" -c "qa"
+	curl -fLo ${XDG_DATA_HOME}/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	nvim -es -u ${XDG_DATA-HOME}/nvim/init.vim -i NONE -c "PlugInstall" -c "qa"
+
+install_scripts:
+	cp -r ./scripts ~/
+	grep -qxF 'PATH=$$PATH:$$HOME/scripts' ${SHELL_INIT} || echo 'PATH=$$PATH:$$HOME/scripts' >> ${SHELL_INIT}
 
 
---
+++ scripts/sublime_py_project.py
@@ -0,0 +1,99 @@
+#! /usr/bin/python3
+# opens a sublime project and sets the correct python interpreter
+
+import argparse
+import json
+import os
+
+try:
+    VIRTUAL_ENV = os.environ["VIRTUAL_ENV"]
+except KeyError:
+    raise Exception("You need a virtual environment to run this script")
+
+
+HOME = os.path.expanduser("~")
+DEFAULT_SUBLIME_PROJECT_DIR = f"{HOME}/projects/sublime/"
+DEFAULT_PROJECT_DIR = f"{HOME}/projects/"
+
+space = argparse.Namespace()
+space.PROJECT_NAME = os.path.basename(VIRTUAL_ENV)
+
+
+def arguments():
+    parser = argparse.ArgumentParser(
+        description="Create a Sublime text python project file"
+    )
+    parser.add_argument(
+        "-p",
+        "--project",
+        nargs="?",
+        default=f"{DEFAULT_PROJECT_DIR}{space.PROJECT_NAME}",
+        help="Directory where the project resides"
+    )
+    parser.add_argument(
+        "-v",
+        "--virtual_env",
+        nargs="?",
+        default=VIRTUAL_ENV,
+        help="Virtual environment location"
+    )
+    parser.add_argument(
+        "-d",
+        "--destination",
+        nargs="?",
+        default=DEFAULT_SUBLIME_PROJECT_DIR,
+        help="Sublime project file directory"
+    )
+
+    return parser.parse_args()
+
+
+def project_file_exists():
+    if os.path.isfile(space.PROJECT_FILE):
+        return True
+    return False
+
+
+def create_project_file():
+    project = {
+        "folders": [
+            {
+                "path": space.args.project,
+            },
+            {   
+                "name": "env",
+                "path": f"{space.args.virtual_env}/lib/python3.9/site-packages/",
+            }
+        ],
+        "settings":
+        {
+            "python_interpreter": f"{space.args.virtual_env}/bin/python"
+        }
+    }
+
+    with open(space.PROJECT_FILE, "w+") as file:
+        file.write(json.dumps(project, indent=4))
+
+
+def print_args():
+    print(f"Project: {space.args.project}")
+    print(f"Virtual_env: {space.args.virtual_env}")
+    print(f"Destination: {space.args.destination}")
+
+
+if __name__ == "__main__":
+    space.args = arguments()
+    space.PROJECT_FILE = f"{space.args.destination}{space.PROJECT_NAME}.sublime-project"
+    shell_cmd = f"subl --project {space.PROJECT_FILE}"
+    if project_file_exists():
+        print(f"Opening: {space.PROJECT_FILE}")
+        os.system(shell_cmd)
+    else:
+        print_args()
+        user_input = input("Sublime Project does not exist, do you want to create a new one? \n[y/N]: ")
+        if user_input.lower().strip() == "y":
+            create_project_file()
+            print(f"Creating new sublime project file at: {space.PROJECT_FILE}")
+            os.system(shell_cmd)
+        else:
+            print("Cya")
